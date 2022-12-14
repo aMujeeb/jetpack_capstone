d65efcd70fb243fff79f2ba149f515aed2f1e688
Mujee <mujee2@gmail.com>
2022-12-14 11:52:15 +0800

    Login screen initiation

--- app/src/main/java/com/mujapps/jetpackcapstone/components/components.kt
+++ app/src/main/java/com/mujapps/jetpackcapstone/components/components.kt
@@ -1,12 +1,20 @@
 package com.mujapps.jetpackcapstone.components
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ReaderLogo(modifier: Modifier = Modifier) {
@@ -15,5 +23,52 @@ fun ReaderLogo(modifier: Modifier = Modifier) {
         modifier = modifier.padding(bottom = 8.dp),
         style = MaterialTheme.typography.h2,
         color = Color.Red.copy(alpha = 0.5f)
+    )
+}
+
+@Composable
+fun EmailInput(
+    modifier: Modifier = Modifier,
+    emailState: MutableState<String>,
+    labelId: String = "Email",
+    enabled: Boolean = true,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    InputField(
+        modifier = modifier,
+        valueState = emailState,
+        labelId = labelId,
+        isEnabled = enabled,
+        keyBoardType = KeyboardType.Email,
+        imeAction = imeAction,
+        onAction = onAction
+    )
+}
+
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    labelId: String,
+    isEnabled: Boolean,
+    isSingleLine: Boolean = true,
+    keyBoardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = {
+            valueState.value = it
+        },
+        modifier = modifier.padding(bottom = 8.dp, start = 12.dp, end = 12.dp),
+        enabled = isEnabled,
+        textStyle = TextStyle(fontSize = 16.sp, color = MaterialTheme.colors.onBackground),
+        keyboardOptions = KeyboardOptions(keyboardType = keyBoardType, imeAction = imeAction),
+        keyboardActions = onAction,
+        label = {
+            Text(text = labelId)
+        }
     )
 }
--- app/src/main/java/com/mujapps/jetpackcapstone/screens/login/LoginScreen.kt
+++ app/src/main/java/com/mujapps/jetpackcapstone/screens/login/LoginScreen.kt
@@ -1,15 +1,17 @@
 package com.mujapps.jetpackcapstone.screens.login
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -17,9 +19,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.mujapps.jetpackcapstone.components.EmailInput
 import com.mujapps.jetpackcapstone.components.ReaderLogo
 
 @Composable
@@ -59,6 +67,13 @@ fun UserForm() {
         .verticalScroll(rememberScrollState())
 
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        EmailInput(emailState = email, enabled = true, onAction = KeyboardActions {
+            passwordFocusedRequest.requestFocus()
+        })
 
+        PasswordInput(
+            modifier = Modifier.focusRequester(passwordFocusedRequest),
+            passwordState = password
+        )
     }
 }
