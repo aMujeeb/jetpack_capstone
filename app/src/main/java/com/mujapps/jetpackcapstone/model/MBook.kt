5051470d4716d1103a8a949a5f3ca7a8d41f0a6a
Mujee <mujee2@gmail.com>
2023-02-07 22:56:53 +0800

    Add top bar

---
+++ app/src/main/java/com/mujapps/jetpackcapstone/model/MBook.kt
@@ -0,0 +1,7 @@
+package com.mujapps.jetpackcapstone.model
+
+data class MBook(
+    var id: String? = null, var title: String? = null,
+    var authors: String? = null,
+    var notes: String? = null
+)
--- app/src/main/java/com/mujapps/jetpackcapstone/screens/home/HomeScreen.kt
+++ app/src/main/java/com/mujapps/jetpackcapstone/screens/home/HomeScreen.kt
@@ -1,13 +1,13 @@
 package com.mujapps.jetpackcapstone.screens.home
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,12 +16,17 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.actionCodeSettings
+import com.mujapps.jetpackcapstone.model.MBook
+import com.mujapps.jetpackcapstone.navigation.ReaderScreens
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
@@ -36,7 +41,17 @@ fun HomeScreen(navController: NavHostController) {
         it.calculateBottomPadding()
         //Content
         Surface(modifier = Modifier.fillMaxSize()) {
+            //Home Content
+            HomeContent(navController)
+        }
+    }
+}
 
+@Composable
+fun HomeContent(navController: NavHostController) {
+    Column(Modifier.padding(4.dp), verticalArrangement = Arrangement.SpaceEvenly) {
+        Row(modifier = Modifier.align(alignment = Alignment.Start)) {
+            TitleSection(label = "Your Reading \n" + " activity right now")
         }
     }
 }
@@ -53,7 +68,6 @@ fun FabContent(onTap: () -> Unit) {
         )
     }
 }
-
 
 @Composable
 fun ReaderAppBar(tittle: String, showProfile: Boolean = true, navController: NavController) {
@@ -75,6 +89,37 @@ fun ReaderAppBar(tittle: String, showProfile: Boolean = true, navController: Nav
                 color = Color.Red.copy(alpha = 0.7f),
                 style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 14.sp)
             )
+            Spacer(modifier = Modifier.width(152.dp))
+
         }
-    }, actions = {}, backgroundColor = Color.Transparent, elevation = 0.dp)
+    }, actions = {
+        IconButton(onClick = {
+            FirebaseAuth.getInstance().signOut().run {
+                navController.navigate(ReaderScreens.LoginScreen.name)
+            }
+        }) {
+            Icon(
+                imageVector = Icons.Filled.Logout,
+                contentDescription = "Log Out",
+                tint = Color.Green.copy(alpha = 0.4f)
+            )
+        }
+    }, backgroundColor = Color.Transparent, elevation = 0.dp)
+}
+
+@Composable
+fun TitleSection(modifier: Modifier = Modifier, label: String) {
+    Surface(modifier = modifier.padding(start = 8.dp, top = 2.dp)) {
+        Text(
+            text = label,
+            fontSize = 18.sp,
+            fontStyle = FontStyle.Normal,
+            textAlign = TextAlign.Left
+        )
+    }
+}
+
+@Composable
+fun ReadingRightNowArea(books: List<MBook>, navController: NavController) {
+
 }
